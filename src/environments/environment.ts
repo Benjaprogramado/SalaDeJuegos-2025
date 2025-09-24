62ca9423ea75081cef25a0db580c2243c9f96826
Benjamin <benjaschell@gmail.com>
2025-09-23 23:51:12 -0300

    Config Proyecto

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB0e0N32LqkWmPeAqw3DBoj2okkxnt76-s",
+    authDomain: "sala-de-juegos-d151e.firebaseapp.com",
+    projectId: "sala-de-juegos-d151e",
+    storageBucket: "sala-de-juegos-d151e.firebasestorage.app",
+    messagingSenderId: "431925622406",
+    appId: "1:431925622406:web:ad37b3e9c80d471afdc307"
+  }
+};
