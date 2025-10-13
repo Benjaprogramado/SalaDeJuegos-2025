d5630e436eecb2a117ba91ca1f6c9763b6507a0b
Benjamin <benjaschell@gmail.com>
2025-10-12 22:22:55 -0300

    Componentes faltantes

---
+++ src/app/components/encuesta/encuesta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-encuesta',
+  imports: [],
+  templateUrl: './encuesta.html',
+  styleUrl: './encuesta.css'
+})
+export class Encuesta {
+
+}
